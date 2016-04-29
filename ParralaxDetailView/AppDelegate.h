71ae00bc8289e226af983172e15451e58d505a05
aman-clickapps <apple@apples-imac.local>
2016-04-29 12:05:52 +0530

    Initial Commit

---
+++ ParralaxDetailView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParralaxDetailView
+//
+//  Created by apple on 29/04/16.
+//  Copyright © 2016 ClickApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParralaxDetailView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParralaxDetailView
+//
+//  Created by apple on 29/04/16.
+//  Copyright © 2016 ClickApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
